61f2d976fb56dac60ba5228ef6c2a1a038128807
Vladislav Gumerov <dev.gumerov@gmail.com>
2021-08-25 17:40:23 +0300

    added Austria flag in activity_flags.xml

--- task_1/src/main/java/com/example/andersenhomework3/FlagsActivity.kt
+++ task_1/src/main/java/com/example/andersenhomework3/FlagsActivity.kt
@@ -3,9 +3,9 @@ package com.example.andersenhomework3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FlagsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.frame_layout)
+        setContentView(R.layout.activity_flags)
     }
 }
